093a6667601b51a1bc9904abdb4c111c6c5dde82
Samuel Jegede <SJegede@spartaglobal.com>
2019-04-23 10:16:00 +0100

    changed tests in ratesdtotest

---
+++ src/test/java/com/sparta/sj/RatesDTOTest.java
@@ -0,0 +1,17 @@
+package com.sparta.sj;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RatesDTOTest {
+
+    @Before
+    public void getJsonFile(){
+
+    }
+
+    @Test
+    public void checkRatesDTO(){
+
+    }
+}
